d62b96a6c2818f70b91656286da5034d21977e20
Anantha Krishna Bhatta <banantha@amazon.com>
2021-04-19 14:21:26 -0700

    Updated licence header
    Also migrated package to org.opensearch
    
    [Tests]
    unit tests and integration tests

---
+++ notifications/src/main/kotlin/org/opensearch/notifications/resthandler/SendMessageRestHandler.kt
@@ -0,0 +1,93 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+
+package org.opensearch.notifications.resthandler
+
+import org.opensearch.client.node.NodeClient
+import org.opensearch.commons.utils.contentParserNextToken
+import org.opensearch.commons.utils.logger
+import org.opensearch.notifications.NotificationPlugin.Companion.PLUGIN_BASE_URI
+import org.opensearch.notifications.action.SendMessageAction
+import org.opensearch.notifications.model.SendMessageRequest
+import org.opensearch.rest.BaseRestHandler
+import org.opensearch.rest.BaseRestHandler.RestChannelConsumer
+import org.opensearch.rest.BytesRestResponse
+import org.opensearch.rest.RestHandler.Route
+import org.opensearch.rest.RestRequest
+import org.opensearch.rest.RestRequest.Method.POST
+import org.opensearch.rest.RestStatus
+
+/**
+ * Rest handler for sending notification.
+ * This handler [SendAction] for sending notification.
+ */
+internal class SendMessageRestHandler : BaseRestHandler() {
+
+    internal companion object {
+        private val log by logger(SendMessageRestHandler::class.java)
+        const val SEND_BASE_URI = "$PLUGIN_BASE_URI/send"
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun getName(): String = "send_message"
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun routes(): List<Route> {
+        return listOf(
+            Route(POST, SEND_BASE_URI)
+        )
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun responseParams(): Set<String> {
+        return setOf()
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    override fun prepareRequest(request: RestRequest, client: NodeClient): RestChannelConsumer {
+        return when (request.method()) {
+            POST -> RestChannelConsumer {
+                client.execute(
+                    SendMessageAction.ACTION_TYPE,
+                    SendMessageRequest(request.contentParserNextToken()),
+                    RestResponseToXContentListener(it)
+                )
+            }
+            else -> RestChannelConsumer {
+                it.sendResponse(BytesRestResponse(RestStatus.METHOD_NOT_ALLOWED, "${request.method()} is not allowed"))
+            }
+        }
+    }
+}
---
+++ notifications/src/test/kotlin/org/opensearch/notifications/channel/SesChannelIT.kt
@@ -0,0 +1,72 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * The OpenSearch Contributors require contributions made to
+ * this file be licensed under the Apache-2.0 license or a
+ * compatible open source license.
+ *
+ * Modifications Copyright OpenSearch Contributors. See
+ * GitHub history for details.
+ */
+
+/*
+ * Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ *
+ */
+
+package org.opensearch.notifications.channel
+
+import org.junit.After
+import org.opensearch.notifications.NotificationsRestTestCase
+import org.opensearch.notifications.getStatusCode
+import org.opensearch.notifications.getStatusText
+import org.opensearch.notifications.jsonify
+import org.opensearch.rest.RestStatus
+
+class SesChannelIT : NotificationsRestTestCase() {
+    private val refTag = "ref"
+    private val title = "title"
+    private val textDescription = "text"
+    private val htmlDescription = "html"
+    private val attachment = jsonify(
+        """
+        {
+          "fileName": "odfe.data",
+          "fileEncoding": "base64",
+          "fileContentType": "application/octet-stream",
+          "fileData": "VGVzdCBtZXNzYWdlCgo="
+        }
+        """.trimIndent()
+    )
+
+    override fun init() {
+        setChannelType("ses")
+    }
+
+    @After
+    fun reset() {
+        resetChannelType()
+    }
+
+    fun `test send email over ses channel due to ses authorization failure`() {
+        val recipients = listOf("mailto:test@localhost")
+        val response = executeRequest(refTag, recipients, title, textDescription, htmlDescription, attachment)
+
+        val statusCode = getStatusCode(response)
+        assertEquals(RestStatus.SERVICE_UNAVAILABLE.status, statusCode)
+
+        val statusText = getStatusText(response)
+        assertEquals("sendEmail Error, SES status:400:Optional[Bad Request]", statusText)
+    }
+}
