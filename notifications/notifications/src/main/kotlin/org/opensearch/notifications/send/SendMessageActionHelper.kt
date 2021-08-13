09e6e37b85af4fcbbbea85dca1256778c56b5901
Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
2021-08-13 13:55:29 -0700

    Adds PublishNotificationAction for legacy ISM implementation (#254)
    * Adds PublishNotificationAction for legacy ISM implementation
    
    Signed-off-by: Drew Baugher <46505179+dbbaughe@users.noreply.github.com>
    
    * Fixes rename of feature and required referenceId from merged changes
    
    Signed-off-by: Drew Baugher <46505179+dbbaughe@users.noreply.github.com>

--- notifications/notifications/src/main/kotlin/org/opensearch/notifications/send/SendMessageActionHelper.kt
+++ notifications/notifications/src/main/kotlin/org/opensearch/notifications/send/SendMessageActionHelper.kt
@@ -16,6 +16,13 @@ import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.runBlocking
 import org.opensearch.OpenSearchStatusException
 import org.opensearch.commons.authuser.User
+import org.opensearch.commons.destination.message.LegacyBaseMessage
+import org.opensearch.commons.destination.message.LegacyCustomWebhookMessage
+import org.opensearch.commons.destination.message.LegacyDestinationType
+import org.opensearch.commons.destination.response.LegacyDestinationResponse
+import org.opensearch.commons.notifications.NotificationConstants.FEATURE_INDEX_MANAGEMENT
+import org.opensearch.commons.notifications.action.LegacyPublishNotificationRequest
+import org.opensearch.commons.notifications.action.LegacyPublishNotificationResponse
 import org.opensearch.commons.notifications.action.SendNotificationRequest
 import org.opensearch.commons.notifications.action.SendNotificationResponse
 import org.opensearch.commons.notifications.model.ChannelMessage
@@ -96,6 +103,19 @@ object SendMessageActionHelper {
         val docId = eventOperations.createNotificationEvent(eventDoc)
             ?: throw OpenSearchStatusException("Indexing not Acknowledged", RestStatus.INSUFFICIENT_STORAGE)
         return SendNotificationResponse(docId)
+    }
+
+    /**
+     * Send legacy notification message intended only for Index Management plugin.
+     * @param request request object
+     */
+    fun executeLegacyRequest(request: LegacyPublishNotificationRequest): LegacyPublishNotificationResponse {
+        val baseMessage = request.baseMessage
+        val response: LegacyDestinationResponse
+        runBlocking {
+            response = sendMessageToLegacyDestination(baseMessage)
+        }
+        return LegacyPublishNotificationResponse(response)
     }
 
     /**
@@ -201,6 +221,51 @@ object SendMessageActionHelper {
             eventStatus.copy(deliveryStatus = DeliveryStatus(RestStatus.NOT_FOUND.name, "Channel not found"))
         } else {
             response
+        }
+    }
+
+    /**
+     * Send message to a legacy destination intended only for Index Management
+     *
+     * Currently this simply converts the legacy base message to the equivalent destination classes that exist
+     * for the notification channels and utilizes the [sendMessageThroughSpi] method. If we get to the point
+     * where this method seems to be holding back notification channels from adding new functionality we can
+     * refactor this to have it's own internal private spi call to completely decouple them instead.
+     *
+     * @param baseMessage legacy base message
+     * @return notification delivery status for the legacy destination
+     */
+    private fun sendMessageToLegacyDestination(baseMessage: LegacyBaseMessage): LegacyDestinationResponse {
+        val message = MessageContent(title = "Index Management Notification", textDescription = baseMessage.messageContent)
+        // These legacy destination calls do not have reference Ids, just passing index management feature constant
+        return when (baseMessage.channelType) {
+            LegacyDestinationType.LEGACY_SLACK -> {
+                val destination = SlackDestination(baseMessage.url)
+                val status = sendMessageThroughSpi(destination, message, FEATURE_INDEX_MANAGEMENT)
+                LegacyDestinationResponse.Builder().withStatusCode(status.statusCode)
+                    .withResponseContent(status.statusText).build()
+            }
+            LegacyDestinationType.LEGACY_CHIME -> {
+                val destination = ChimeDestination(baseMessage.url)
+                val status = sendMessageThroughSpi(destination, message, FEATURE_INDEX_MANAGEMENT)
+                LegacyDestinationResponse.Builder().withStatusCode(status.statusCode)
+                    .withResponseContent(status.statusText).build()
+            }
+            LegacyDestinationType.LEGACY_CUSTOM_WEBHOOK -> {
+                val destination = CustomWebhookDestination(
+                    (baseMessage as LegacyCustomWebhookMessage).uri.toString(),
+                    baseMessage.headerParams,
+                    baseMessage.method
+                )
+                val status = sendMessageThroughSpi(destination, message, FEATURE_INDEX_MANAGEMENT)
+                LegacyDestinationResponse.Builder().withStatusCode(status.statusCode)
+                    .withResponseContent(status.statusText).build()
+            }
+            null -> {
+                log.warn("No channel type given (null) for publishing to legacy destination")
+                LegacyDestinationResponse.Builder().withStatusCode(400)
+                    .withResponseContent("No channel type given (null) for publishing to legacy destination").build()
+            }
         }
     }
 
