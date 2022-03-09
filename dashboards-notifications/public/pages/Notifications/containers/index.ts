c1ceadb8d4585290256249b23d97e565f018eabd
Mohammad Qureshi <47198598+qreshi@users.noreply.github.com>
2022-03-09 10:35:26 -0800

    Remove sent Notifications dashboard
    Signed-off-by: Mohammad Qureshi <47198598+qreshi@users.noreply.github.com>

--- dashboards-notifications/public/pages/Notifications/containers/index.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import Notifications from './Notifications';
-
-export default Notifications;
--- dashboards-notifications/public/pages/Notifications/utils/constants.ts
+++ dashboards-notifications/public/pages/Notifications/utils/constants.ts
@@ -3,19 +3,4 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { SortDirection } from '@elastic/eui';
-import { HISTOGRAM_TYPE } from '../../../utils/constants';
-
 export const DEFAULT_PAGE_SIZE_OPTIONS = [5, 10, 20, 50];
-
-export const DEFAULT_QUERY_PARAMS = {
-  from: 0,
-  search: '',
-  size: 10,
-  sortDirection: SortDirection.DESC,
-  sortField: 'last_updated_time_ms',
-  startTime: 'now-7d',
-  endTime: 'now',
-  filters: [],
-  histogramType: Object.values(HISTOGRAM_TYPE)[0],
-};
