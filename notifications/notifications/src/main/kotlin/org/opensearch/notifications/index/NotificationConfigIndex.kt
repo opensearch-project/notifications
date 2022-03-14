b5d6f11cdb4409e7e1c2486376a7837ee0a505bd
Aditya Jindal <aditjind@amazon.com>
2022-03-14 13:58:28 -0700

    Changes to Notification APIs
    Updates to createNotificationConfig API
    
    Signed-off-by: Aditya Jindal <aditjind@amazon.com>
    
    Updates to getNotificationConfigs API
    
    Signed-off-by: Aditya Jindal <aditjind@amazon.com>
    
    Updates to createIndex API
    
    Signed-off-by: Aditya Jindal <aditjind@amazon.com>
    
    Updates to getNotificationConfig API
    
    Signed-off-by: Aditya Jindal <aditjind@amazon.com>
    
    Updates to getAllNotificationConfigs API
    
    Signed-off-by: Aditya Jindal <aditjind@amazon.com>
    
    Updates to updateNotificationConfig API
    
    Signed-off-by: Aditya Jindal <aditjind@amazon.com>
    
    Updates to deleteNotificationConfig API
    
    Signed-off-by: Aditya Jindal <aditjind@amazon.com>
    
    Updates to deleteNotificationConfigs APIs
    
    Signed-off-by: Aditya Jindal <aditjind@amazon.com>

--- notifications/notifications/src/main/kotlin/org/opensearch/notifications/index/NotificationConfigIndex.kt
+++ notifications/notifications/src/main/kotlin/org/opensearch/notifications/index/NotificationConfigIndex.kt
@@ -5,6 +5,11 @@
 
 package org.opensearch.notifications.index
 
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
 import org.opensearch.ResourceAlreadyExistsException
 import org.opensearch.action.DocWriteResponse
 import org.opensearch.action.admin.indices.create.CreateIndexRequest
@@ -14,6 +19,7 @@ import org.opensearch.action.get.GetRequest
 import org.opensearch.action.get.GetResponse
 import org.opensearch.action.get.MultiGetRequest
 import org.opensearch.action.index.IndexRequest
+import org.opensearch.action.index.IndexResponse
 import org.opensearch.action.search.SearchRequest
 import org.opensearch.client.Client
 import org.opensearch.cluster.service.ClusterService
@@ -41,6 +47,11 @@ import org.opensearch.search.SearchHit
 import org.opensearch.search.builder.SearchSourceBuilder
 import org.opensearch.search.sort.SortOrder
 import java.util.concurrent.TimeUnit
+import org.opensearch.action.bulk.BulkResponse
+import org.opensearch.action.delete.DeleteResponse
+import org.opensearch.action.get.MultiGetResponse
+import org.opensearch.action.search.SearchResponse
+import org.opensearch.action.support.master.AcknowledgedResponse
 
 /**
  * Class for doing index operations to maintain configurations in cluster.
@@ -91,13 +102,23 @@ internal object NotificationConfigIndex : ConfigOperations {
             val classLoader = NotificationConfigIndex::class.java.classLoader
             val indexMappingSource = classLoader.getResource(MAPPING_FILE_NAME)?.readText()!!
             val indexSettingsSource = classLoader.getResource(SETTINGS_FILE_NAME)?.readText()!!
-            val request = CreateIndexRequest(INDEX_NAME)
-                .mapping(MAPPING_TYPE, indexMappingSource, XContentType.YAML)
-                .settings(indexSettingsSource, XContentType.YAML)
             try {
-                val actionFuture = client.admin().indices().create(request)
-                val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
-                if (response.isAcknowledged) {
+                val deferred = mutableListOf<Deferred<AcknowledgedResponse>>()
+                runBlocking {
+                    deferred.add(
+                        async(Dispatchers.IO) {
+                            val request = CreateIndexRequest(INDEX_NAME)
+                                .mapping(MAPPING_TYPE, indexMappingSource, XContentType.YAML)
+                                .settings(indexSettingsSource, XContentType.YAML)
+                            val actionFuture = client.admin().indices().create(request)
+                            val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
+                            response
+                        }
+                    )
+                    deferred.awaitAll()
+                }
+
+                if (deferred.get(0).getCompleted().isAcknowledged) {
                     log.info("$LOG_PREFIX:Index $INDEX_NAME creation Acknowledged")
                 } else {
                     throw IllegalStateException("$LOG_PREFIX:Index $INDEX_NAME creation not Acknowledged")
@@ -124,19 +145,28 @@ internal object NotificationConfigIndex : ConfigOperations {
      */
     override fun createNotificationConfig(configDoc: NotificationConfigDoc, id: String?): String? {
         createIndex()
-        val indexRequest = IndexRequest(INDEX_NAME)
-            .source(configDoc.toXContent())
-            .create(true)
-        if (id != null) {
-            indexRequest.id(id)
+        val deferred = mutableListOf<Deferred<IndexResponse>>()
+        runBlocking {
+            deferred.add(
+                async(Dispatchers.IO) {
+                    val indexRequest = IndexRequest(INDEX_NAME)
+                        .source(configDoc.toXContent())
+                        .create(true)
+                    if (id != null) {
+                        indexRequest.id("$id")
+                    }
+                    val actionFuture = client.index(indexRequest)
+                    val res = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
+                    res
+                }
+            )
+            deferred.awaitAll()
         }
-        val actionFuture = client.index(indexRequest)
-        val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
-        return if (response.result != DocWriteResponse.Result.CREATED) {
-            log.warn("$LOG_PREFIX:createNotificationConfig - response:$response")
+        return if (deferred.get(0).getCompleted().result != DocWriteResponse.Result.CREATED) {
+            log.warn("$LOG_PREFIX:createNotificationConfig - response:$deferred.get(0)")
             null
         } else {
-            response.id
+            deferred.get(0).getCompleted().id
         }
     }
 
@@ -145,11 +175,33 @@ internal object NotificationConfigIndex : ConfigOperations {
      */
     override fun getNotificationConfigs(ids: Set<String>): List<NotificationConfigDocInfo> {
         createIndex()
-        val getRequest = MultiGetRequest()
-        ids.forEach { getRequest.add(INDEX_NAME, it) }
-        val actionFuture = client.multiGet(getRequest)
-        val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
-        return response.responses.mapNotNull { parseNotificationConfigDoc(it.id, it.response) }
+        val deferred = mutableListOf<Deferred<MultiGetResponse>>()
+        runBlocking {
+            deferred.add(
+                async(Dispatchers.IO) {
+                    val getRequest = MultiGetRequest()
+                    ids.forEach { getRequest.add(INDEX_NAME, it) }
+                    val actionFuture = client.multiGet(getRequest)
+                    val res = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
+                    res
+                }
+            )
+            deferred.awaitAll()
+        }
+
+        val response : List<NotificationConfigDocInfo> = emptyList()
+
+        response.map {
+            deferred.forEach {
+                it.getCompleted().responses.iterator().forEach {
+                    parseNotificationConfigDoc(
+                        it.id,
+                        it.response
+                    )
+                }
+            }
+        }
+        return response
     }
 
     /**
@@ -157,10 +209,20 @@ internal object NotificationConfigIndex : ConfigOperations {
      */
     override fun getNotificationConfig(id: String): NotificationConfigDocInfo? {
         createIndex()
-        val getRequest = GetRequest(INDEX_NAME).id(id)
-        val actionFuture = client.get(getRequest)
-        val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
-        return parseNotificationConfigDoc(id, response)
+        val deferred = mutableListOf<Deferred<GetResponse>>()
+        runBlocking {
+            deferred.add(
+                async(Dispatchers.IO) {
+                    val getRequest = GetRequest(INDEX_NAME).id(id)
+                    val actionFuture = client.get(getRequest)
+                    val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
+                    response
+                }
+            )
+            deferred.awaitAll()
+        }
+
+        return parseNotificationConfigDoc(id, deferred.get(0).getCompleted())
     }
 
     private fun parseNotificationConfigDoc(id: String, response: GetResponse): NotificationConfigDocInfo? {
@@ -193,23 +255,33 @@ internal object NotificationConfigIndex : ConfigOperations {
         request: GetNotificationConfigRequest
     ): NotificationConfigSearchResult {
         createIndex()
-        val sourceBuilder = SearchSourceBuilder()
-            .timeout(TimeValue(PluginSettings.operationTimeoutMs, TimeUnit.MILLISECONDS))
-            .sort(getSortField(request.sortField), request.sortOrder ?: SortOrder.ASC)
-            .size(request.maxItems)
-            .from(request.fromIndex)
-        val query = QueryBuilders.boolQuery()
-        if (access.isNotEmpty()) {
-            query.filter(QueryBuilders.termsQuery("$METADATA_TAG.$ACCESS_LIST_TAG", access))
+        val deferred = mutableListOf<Deferred<SearchResponse>>()
+        runBlocking {
+            deferred.add(
+                async(Dispatchers.IO) {
+                    val sourceBuilder = SearchSourceBuilder()
+                        .timeout(TimeValue(PluginSettings.operationTimeoutMs, TimeUnit.MILLISECONDS))
+                        .sort(getSortField(request.sortField), request.sortOrder ?: SortOrder.ASC)
+                        .size(request.maxItems)
+                        .from(request.fromIndex)
+                    val query = QueryBuilders.boolQuery()
+                    if (access.isNotEmpty()) {
+                        query.filter(QueryBuilders.termsQuery("$METADATA_TAG.$ACCESS_LIST_TAG", access))
+                    }
+                    ConfigQueryHelper.addQueryFilters(query, request.filterParams)
+                    sourceBuilder.query(query)
+                    val searchRequest = SearchRequest()
+                        .indices(INDEX_NAME)
+                        .source(sourceBuilder)
+                    val actionFuture = client.search(searchRequest)
+                    val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
+                    response
+                }
+            )
+            deferred.awaitAll()
         }
-        ConfigQueryHelper.addQueryFilters(query, request.filterParams)
-        sourceBuilder.query(query)
-        val searchRequest = SearchRequest()
-            .indices(INDEX_NAME)
-            .source(sourceBuilder)
-        val actionFuture = client.search(searchRequest)
-        val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
-        val result = NotificationConfigSearchResult(request.fromIndex.toLong(), response, searchHitParser)
+
+        val result = NotificationConfigSearchResult(request.fromIndex.toLong(), deferred.get(0).getCompleted(), searchHitParser)
         log.info(
             "$LOG_PREFIX:getAllNotificationConfigs from:${request.fromIndex}, maxItems:${request.maxItems}," +
                 " sortField:${request.sortField}, sortOrder=${request.sortOrder}, filters=${request.filterParams}" +
@@ -223,16 +295,25 @@ internal object NotificationConfigIndex : ConfigOperations {
      */
     override fun updateNotificationConfig(id: String, notificationConfigDoc: NotificationConfigDoc): Boolean {
         createIndex()
-        val indexRequest = IndexRequest(INDEX_NAME)
-            .source(notificationConfigDoc.toXContent())
-            .create(false)
-            .id(id)
-        val actionFuture = client.index(indexRequest)
-        val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
-        if (response.result != DocWriteResponse.Result.UPDATED) {
-            log.warn("$LOG_PREFIX:updateNotificationConfig failed for $id; response:$response")
+        val deferred = mutableListOf<Deferred<IndexResponse>>()
+        runBlocking {
+            deferred.add(
+                async(Dispatchers.IO) {
+                    val indexRequest = IndexRequest(INDEX_NAME)
+                        .source(notificationConfigDoc.toXContent())
+                        .create(false)
+                        .id(id)
+                    val actionFuture = client.index(indexRequest)
+                    val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
+                    response
+                }
+            )
+            deferred.awaitAll()
         }
-        return response.result == DocWriteResponse.Result.UPDATED
+        if (deferred.get(0).getCompleted().result != DocWriteResponse.Result.UPDATED) {
+            log.warn("$LOG_PREFIX:updateNotificationConfig failed for $id; response:$deferred.get(0).getCompleted()")
+        }
+        return deferred.get(0).getCompleted().result == DocWriteResponse.Result.UPDATED
     }
 
     /**
@@ -240,15 +321,25 @@ internal object NotificationConfigIndex : ConfigOperations {
      */
     override fun deleteNotificationConfig(id: String): Boolean {
         createIndex()
-        val deleteRequest = DeleteRequest()
-            .index(INDEX_NAME)
-            .id(id)
-        val actionFuture = client.delete(deleteRequest)
-        val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
-        if (response.result != DocWriteResponse.Result.DELETED) {
-            log.warn("$LOG_PREFIX:deleteNotificationConfig failed for $id; response:$response")
+        val deferred = mutableListOf<Deferred<DeleteResponse>>()
+        runBlocking {
+            deferred.add(
+                async(Dispatchers.IO) {
+                    val deleteRequest = DeleteRequest()
+                        .index(INDEX_NAME)
+                        .id(id)
+                    val actionFuture = client.delete(deleteRequest)
+                    val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
+                    response
+                }
+            )
+            deferred.awaitAll()
         }
-        return response.result == DocWriteResponse.Result.DELETED
+
+        if (deferred.get(0).getCompleted().result != DocWriteResponse.Result.DELETED) {
+            log.warn("$LOG_PREFIX:deleteNotificationConfig failed for $id; response:$deferred.get(0).getCompleted()")
+        }
+        return deferred.get(0).getCompleted().result == DocWriteResponse.Result.DELETED
     }
 
     /**
@@ -256,17 +347,29 @@ internal object NotificationConfigIndex : ConfigOperations {
      */
     override fun deleteNotificationConfigs(ids: Set<String>): Map<String, RestStatus> {
         createIndex()
-        val bulkRequest = BulkRequest()
-        ids.forEach {
-            val deleteRequest = DeleteRequest()
-                .index(INDEX_NAME)
-                .id(it)
-            bulkRequest.add(deleteRequest)
+
+        val deferred = mutableListOf<Deferred<BulkResponse>>()
+        runBlocking {
+            deferred.add(
+                async(Dispatchers.IO) {
+                    val bulkRequest = BulkRequest()
+                    ids.forEach {
+                        val deleteRequest = DeleteRequest()
+                            .index(INDEX_NAME)
+                            .id(it)
+                        bulkRequest.add(deleteRequest)
+                    }
+                    val actionFuture = client.bulk(bulkRequest)
+                    val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
+                    response
+                }
+            )
+            deferred.awaitAll()
         }
-        val actionFuture = client.bulk(bulkRequest)
-        val response = actionFuture.actionGet(PluginSettings.operationTimeoutMs)
+
         val mutableMap = mutableMapOf<String, RestStatus>()
-        response.forEach {
+
+        deferred.get(0).getCompleted().forEach {
             mutableMap[it.id] = it.status()
             if (it.isFailed) {
                 log.warn("$LOG_PREFIX:deleteNotificationConfig failed for ${it.id}; response:${it.failureMessage}")
