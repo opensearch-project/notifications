2f5af905bd19902a9d944f8168b38d2d15e9d3a9
Ashish Agrawal <ashisagr@amazon.com>
2022-08-04 18:14:49 -0700

    Add security tests and workflow plus minor fix (#470)
    * Add security tests and workflow plus minor fix
    
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>
    
    * fix test and update workflow
    
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>
    
    * apply cleanup comments
    
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>
    (cherry picked from commit 7f3c6a89e319a2f5937c4f8d0bfe5bd9f669c49e)

--- notifications/notifications/src/test/kotlin/org/opensearch/integtest/features/GetNotificationChannelListIT.kt
+++ notifications/notifications/src/test/kotlin/org/opensearch/integtest/features/GetNotificationChannelListIT.kt
@@ -5,117 +5,14 @@
 
 package org.opensearch.integtest.features
 
-import com.google.gson.JsonObject
-import org.junit.Assert
 import org.opensearch.commons.notifications.model.ConfigType
 import org.opensearch.integtest.PluginRestTestCase
 import org.opensearch.notifications.NotificationPlugin.Companion.PLUGIN_BASE_URI
+import org.opensearch.notifications.verifyChannelIdEquals
 import org.opensearch.rest.RestRequest
 import org.opensearch.rest.RestStatus
-import kotlin.random.Random
 
 class GetNotificationChannelListIT : PluginRestTestCase() {
-    private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-
-    private fun getCreateRequestJsonString(
-        nameSubstring: String,
-        configType: ConfigType,
-        isEnabled: Boolean,
-        smtpAccountId: String = "",
-        emailGroupId: Set<String> = setOf()
-    ): String {
-        val randomString = (1..20)
-            .map { Random.nextInt(0, charPool.size) }
-            .map(charPool::get)
-            .joinToString("")
-        val configObjectString = when (configType) {
-            ConfigType.SLACK -> """
-                "slack":{"url":"https://slack.domain.com/sample_slack_url#$randomString"}
-            """.trimIndent()
-            ConfigType.CHIME -> """
-                "chime":{"url":"https://chime.domain.com/sample_chime_url#$randomString"}
-            """.trimIndent()
-            ConfigType.WEBHOOK -> """
-                "webhook":{"url":"https://web.domain.com/sample_web_url#$randomString"}
-            """.trimIndent()
-            ConfigType.SMTP_ACCOUNT -> """
-                "smtp_account":{
-                    "host":"smtp.domain.com",
-                    "port":"4321",
-                    "method":"ssl",
-                    "from_address":"$randomString@from.com"
-                }
-            """.trimIndent()
-            ConfigType.EMAIL_GROUP -> """
-                "email_group":{
-                    "recipient_list":[
-                        {"recipient":"$randomString+recipient1@from.com"},
-                        {"recipient":"$randomString+recipient2@from.com"}
-                    ]
-                }
-            """.trimIndent()
-            ConfigType.EMAIL -> """
-                "email":{
-                    "email_account_id":"$smtpAccountId",
-                    "recipient_list":[{"recipient":"$randomString@from.com"}],
-                    "email_group_id_list":[${emailGroupId.joinToString { "\"$it\"" }}]
-                }
-            """.trimIndent()
-            else -> throw IllegalArgumentException("Unsupported configType=$configType")
-        }
-        return """
-        {
-            "config_id":"$randomString",
-            "config":{
-                "name":"$nameSubstring:this is a sample config name $randomString",
-                "description":"this is a sample config description $randomString",
-                "config_type":"$configType",
-                "is_enabled":$isEnabled,
-                $configObjectString
-            }
-        }
-        """.trimIndent()
-    }
-
-    private fun createConfig(
-        nameSubstring: String = "",
-        configType: ConfigType = ConfigType.SLACK,
-        isEnabled: Boolean = true,
-        smtpAccountId: String = "",
-        emailGroupId: Set<String> = setOf()
-    ): String {
-        val createRequestJsonString = getCreateRequestJsonString(
-            nameSubstring,
-            configType,
-            isEnabled,
-            smtpAccountId,
-            emailGroupId
-        )
-        val createResponse = executeRequest(
-            RestRequest.Method.POST.name,
-            "$PLUGIN_BASE_URI/configs",
-            createRequestJsonString,
-            RestStatus.OK.status
-        )
-        val configId = createResponse.get("config_id").asString
-        Assert.assertNotNull(configId)
-        Thread.sleep(100)
-        return configId
-    }
-
-    private fun verifyChannelIdEquals(idSet: Set<String>, jsonObject: JsonObject, totalHits: Int = -1) {
-        if (totalHits >= 0) {
-            Assert.assertEquals(totalHits, jsonObject.get("total_hits").asInt)
-        }
-        val items = jsonObject.get("channel_list").asJsonArray
-        Assert.assertEquals(idSet.size, items.size())
-        items.forEach {
-            val item = it.asJsonObject
-            val configId = item.get("config_id").asString
-            Assert.assertNotNull(configId)
-            Assert.assertTrue(idSet.contains(configId))
-        }
-    }
 
     fun `test POST channel list should result in error`() {
         executeRequest(
