3cb999a092076f5f750ebf611a5d7bdca666aa22
Ashish Agrawal <ashisagr@amazon.com>
2022-10-11 10:06:50 -0700

    Add support for LEGACY SNS
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>

--- notifications/notifications/src/main/kotlin/org/opensearch/notifications/send/SendMessageActionHelper.kt
+++ notifications/notifications/src/main/kotlin/org/opensearch/notifications/send/SendMessageActionHelper.kt
@@ -16,6 +16,7 @@ import org.opensearch.commons.destination.message.LegacyBaseMessage
 import org.opensearch.commons.destination.message.LegacyCustomWebhookMessage
 import org.opensearch.commons.destination.message.LegacyDestinationType
 import org.opensearch.commons.destination.message.LegacyEmailMessage
+import org.opensearch.commons.destination.message.LegacySNSMessage
 import org.opensearch.commons.destination.response.LegacyDestinationResponse
 import org.opensearch.commons.notifications.action.LegacyPublishNotificationRequest
 import org.opensearch.commons.notifications.action.LegacyPublishNotificationResponse
@@ -330,6 +331,13 @@ object SendMessageActionHelper {
                     LegacyDestinationResponse.Builder().withStatusCode(overallStatus.toInt())
                         .withResponseContent(overallStatusText).build()
                 }
+            }
+            LegacyDestinationType.LEGACY_SNS -> {
+                val legacySnsMessage = baseMessage as LegacySNSMessage
+                val destination = SnsDestination(legacySnsMessage.topicArn, legacySnsMessage.roleArn)
+                val status = sendMessageThroughSpi(destination, message, "legacy")
+                LegacyDestinationResponse.Builder().withStatusCode(status.statusCode)
+                    .withResponseContent(status.statusText).build()
             }
             null -> {
                 log.warn("No channel type given (null) for publishing to legacy destination")
