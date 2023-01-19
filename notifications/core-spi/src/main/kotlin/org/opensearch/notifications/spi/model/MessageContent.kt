8a81ae0f15921f2e768f3f6cb5a47aff3cd3725a
danielkyalo599 <johbiento@gmail.com>
2023-01-19 18:45:11 +0300

    FEATURE Alerting mail support html format
    Signed-off-by: danielkyalo599 <johbiento@gmail.com>

--- notifications/core-spi/src/main/kotlin/org/opensearch/notifications/spi/model/MessageContent.kt
+++ notifications/core-spi/src/main/kotlin/org/opensearch/notifications/spi/model/MessageContent.kt
@@ -12,21 +12,28 @@ import org.opensearch.common.Strings
  */
 @SuppressWarnings("LongParameterList")
 class MessageContent(
-    val title: String,
-    val textDescription: String,
-    val htmlDescription: String? = null,
-    val fileName: String? = null,
-    val fileEncoding: String? = null,
-    val fileData: String? = null,
-    val fileContentType: String? = null
+        val title: String,
+        val textDescription: String,
+        val htmlDescription: String? = null,
+        val fileName: String? = null,
+        val fileEncoding: String? = null,
+        val fileData: String? = null,
+        val fileContentType: String? = null
 ) {
 
     init {
         require(!Strings.isNullOrEmpty(title)) { "title is null or empty" }
         require(!Strings.isNullOrEmpty(textDescription)) { "text message part is null or empty" }
+        require(!Strings.isNullOrEmpty(htmlDescription)) { "html message part is null or empty" }
+        if (fileName != null) {
+            require(!Strings.isNullOrEmpty(fileName)) { "file name is null or empty" }
+            require(!Strings.isNullOrEmpty(fileEncoding)) { "file encoding is null or empty" }
+            require(!Strings.isNullOrEmpty(fileData)) { "file data is null or empty" }
+            require(!Strings.isNullOrEmpty(fileContentType)) { "file content type is null or empty" }
+        }
     }
 
     fun buildMessageWithTitle(): String {
-        return "$title\n\n$textDescription"
+        return "$title\n\n$textDescription\n\n$htmlDescription"
     }
 }
