f5174ad9e42be6823bb8a776df51acf00a181c87
danielkyalo599 <johbiento@gmail.com>
2023-02-13 10:24:30 +0300

    Removed hard coded url
    Signed-off-by: danielkyalo599 <johbiento@gmail.com>

--- notifications/core-spi/src/main/kotlin/org/opensearch/notifications/spi/model/destination/TelegramDestination.kt
+++ notifications/core-spi/src/main/kotlin/org/opensearch/notifications/spi/model/destination/TelegramDestination.kt
@@ -8,7 +8,5 @@ package org.opensearch.notifications.spi.model.destination
 This class holds the contents of a Telegram  destination
  */
 class TelegramDestination(
-    val token: String,
-    val chatId: Long,
-    val url: String = "https://api.telegram.org/bot$token/sendMessage?chat_id=$chatId"
+    url: String
 ) : WebhookDestination(url, DestinationType.TELEGRAM)
