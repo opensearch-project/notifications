4b037fc44007c45c1fee47f12fefccca79885a1c
Ashish Agrawal <ashisagr@amazon.com>
2023-03-03 14:39:40 -0800

    Remove dashboards (#640)
    Signed-off-by: Ashish Agrawal <ashisagr@amazon.com>

--- dashboards-notifications/public/components/Modal/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import { ModalConsumer, ModalProvider } from './Modal';
-import ModalRoot from './ModalRoot';
-
-export { ModalConsumer, ModalProvider, ModalRoot };
