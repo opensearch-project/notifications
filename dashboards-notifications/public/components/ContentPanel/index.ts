ac6193875b076eabacb7b5595343966efb219749
Angie Zhang <langelzh@amazon.com>
2023-03-28 18:07:28 -0700

    [Backport 2.x] Remove dashboards (#647)
    * resolve confilict
    
    Signed-off-by: Angie Zhang <langelzh@amazon.com>
    
    * Backport #640
    
    Signed-off-by: Angie Zhang <langelzh@amazon.com>
    
    * Backport #640
    
    Signed-off-by: Angie Zhang <langelzh@amazon.com>
    
    ---------
    
    Signed-off-by: Angie Zhang <langelzh@amazon.com>
    Co-authored-by: Ashish Agrawal <ashisagr@amazon.com>

--- dashboards-notifications/public/components/ContentPanel/index.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright OpenSearch Contributors
- * SPDX-License-Identifier: Apache-2.0
- */
-
-import ContentPanel from './ContentPanel';
-import ContentPanelActions from './ContentPanelActions';
-
-export { ContentPanel, ContentPanelActions };
