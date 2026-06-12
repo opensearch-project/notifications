4c5a85f4680a1d3971942c195055780c028a9196
Mark Boyd <mark.boyd@gsa.gov>
2026-06-12 17:30:07 -0400

    create stub of integration test for access to email notifications
    Signed-off-by: Mark Boyd <mark.boyd@gsa.gov>

---
+++ notifications/notifications/src/test/kotlin/org/opensearch/integtest/EmailNotificationAccessIT.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.opensearch.integtest
+
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.BeforeClass
+import org.opensearch.client.RestClient
+import org.opensearch.commons.rest.SecureRestClientBuilder
+import org.opensearch.core.rest.RestStatus
+import org.opensearch.notifications.NotificationPlugin
+import org.opensearch.notifications.verifySingleConfigEquals
+import org.opensearch.rest.RestRequest
+
+class EmailNotificationAccessIT : PluginRestTestCase() {
+
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun setup() {
+            // things to execute once and keep around for the class
+            org.junit.Assume.assumeTrue(System.getProperty("https", "false")!!.toBoolean())
+        }
+    }
+
+    private val user = "emailNotificationTestUser"
+    private val password = randomAlphaOfLength(6) + "_" + randomIntBetween(1000, 10000) + "!" + randomAlphaOfLength(10)
+    var userClient: RestClient? = null
+
+    @Before
+    fun create() {
+        createUser(user, password, arrayOf())
+        userClient = SecureRestClientBuilder(clusterHosts.toTypedArray(), isHttps(), user, password)
+            .setSocketTimeout(60000)
+            .setConnectionRequestTimeout(180000)
+            .build()
+    }
+
+    @After
+    fun cleanup() {
+        userClient?.close()
+        userClient = null
+    }
+
+    fun `test Create email notification config with user that has create Notification permission`() {
+        createUserWithCustomRole(user, password, NOTIFICATION_CREATE_CONFIG_ACCESS, arrayOf(""), ROLE_TO_PERMISSION_MAPPING[NOTIFICATION_CREATE_CONFIG_ACCESS])
+
+        val (emailConfig, createEmailNotificationJsonString) = createTestEmailNotification()
+
+        try {
+            val configId = createConfigWithRequestJsonString(createEmailNotificationJsonString, userClient!!)
+            Assert.assertNotNull(configId)
+            Thread.sleep(1000)
+
+            // Get Slack notification config
+            val getConfigResponse = executeRequest(
+                RestRequest.Method.GET.name,
+                "${NotificationPlugin.PLUGIN_BASE_URI}/configs/$configId",
+                "",
+                RestStatus.OK.status
+            )
+            verifySingleConfigEquals(configId, emailConfig, getConfigResponse)
+        } finally {
+            deleteUserWithCustomRole(user, NOTIFICATION_CREATE_CONFIG_ACCESS)
+        }
+    }
+}
